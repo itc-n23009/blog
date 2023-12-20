201137f0c53d53f2113031ba0eb127a1660843e5
itc-n23009 <n23009@std.it-college.ac.jp>
2023-12-20 06:34:47 +0000

    p128~131 finish
    文とコンタクト情報を分離させた

--- pages/about.js
+++ pages/about.js
@@ -2,12 +2,15 @@ import Container from 'components/container'
 import Hero from 'components/hero'
 import PostBody from 'components/post-body'
 import Contact from 'components/contact'
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-column'
 
 const About = () => {
  return (
     <Container>
       <Hero title="About" subtitle="About development activities" />
 
+<TwoColumn>
+   <TwoColumnMain>
    <PostBody>
    <p>
     Cubeが得意とする分野はモノづくりです。３次元から２次元の造形、プログラミングやデザインなど、さまざまな技術を組み合わせることによって社会や環境と結びつけるクリエイティブを提案し続けています。
@@ -24,7 +27,12 @@ const About = () => {
     今までと違うものを作ることで愛着が湧いてきます。そこで興味を持ったことは小さなことでもいいから取り入れて、良いものを作れるようにしています。小さなヒントから新しいものを生み出すようなモノづくりは、これからも続けていきたいです。
    </p>
    </PostBody>
+   </TwoColumnMain>
+
+   <TwoColumnSidebar>
    <Contact />
+   </TwoColumnSidebar>
+   </TwoColumn>
     </Container>
 )
 }
