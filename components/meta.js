122debceb00247fbb2d45406fb3dc5a379e0d1d8
itc-n23009 <n23009@std.it-college.ac.jp>
2023-12-22 01:49:00 +0000

    p191~193 finish
    各ページのタイトルを変更した

---
+++ components/meta.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+import { siteMeta } from 'lib/constants'
+const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } = siteMeta
+
+const Meta = ({ pageTitle }) => {
+  const title = pageTitle ? '${pageTitle} | ${siteTitle}' : siteTitle
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta property="og:title" content={title} />
+    </Head>
+  )
+}
+export default Meta
